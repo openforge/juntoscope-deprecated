69d1139f85e08cc4bacbfb9e692427d42ac607bd
Meredith Alcorn <meredithalcorn@Merediths-MacBook-Pro.local>
2018-01-15 17:42:30 -0500

    Add votes-counted-component

--- src/components/votes-counted-component/votes-counted-component.tsx
+++ src/components/votes-counted-component/votes-counted-component.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, Prop, State, Event, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'votes-counted-component',
@@ -7,10 +7,38 @@ import { Component } from '@stencil/core';
 
 export class VotesCountedComponent {
 
+  @Prop() value: any;
+
+  @Prop() description: string;
+
+  @Prop() isCustom: boolean = false;
+
+  @State() customVoteValue: any;
+
+  @Event() onCustomVote: EventEmitter;
+
+  handleChange(event) {
+    this.customVoteValue = event.target.value
+    console.log(this.customVoteValue)
+    this.onCustomVote.emit(this.customVoteValue);
+  }
+
   render(){
-    return (
-      <div>
-      </div>
-    )
+    if (this.isCustom) {
+      return (
+        <form>
+         <input type="number" placeholder="--" value={this.customVoteValue} class="box" onInput={() => this.handleChange(event)} />
+         <label>{this.description}</label>
+        </form>
+      )
+    }
+    else {
+      return (
+        <div class="box">
+          <p>{this.value}</p>
+          <p>{this.description}</p>
+        </div>
+      )
+    }
   }
 }
