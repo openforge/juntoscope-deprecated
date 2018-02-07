563d183007b455162c444cfbb0ca602133df384e
Meredith Alcorn <meredithalcorn@Merediths-MacBook-Pro.local>
2018-02-07 17:32:23 -0500

    create room for projects; emit events for tasks and votes to the room.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var app = require('express')();
 var path = require('path');
-var serveStatic = require('serve-static')
+var serveStatic = require('serve-static');
 
-var http = require('http').Server(app);
+var http = require('http').createServer( app);
 var io = require('socket.io')(http);
+
+const projects = {};
 
 app.use(serveStatic(path.join(__dirname, '../www'), {
 }))
@@ -13,16 +15,52 @@ app.get('/**', (req, res) => {
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected!');
+  console.log('socket connected!', socket.id);
 
-  socket.on('tasks', (task) => {
-    console.log('task', task)
-    // io.sockets emits to all of the sockets
-    io.sockets.emit('tasks', task);
-  });
+  socket.on('joinRoom', function(projectName) {
+    socket.join(projectName);
+    console.log('joined room', projectName);
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+    if(projects[projectName]) {
+      projects[projectName].users++;
+    } else {
+      projects[projectName] = {
+        users: 1,
+        currentTask: null,
+        tasks: []
+      };
+    }
+
+    io.sockets.in(projectName).emit('projectData', projects[projectName])
+
+    socket.on('tasks', (tasks) => {
+      console.log('logging task from server', tasks);
+
+      projects[projectName].tasks = tasks.map((task) => {
+        return {
+          name: task,
+          currentTask: task,
+          votes: []
+        }
+      });
+
+      io.sockets.in(projectName).emit('tasks', projects[projectName].tasks)
+    });
+
+    socket.on('votes', (vote) => {
+      // Whenever someone votes, I need to add the vote to the votes array
+      // Once all users have voted -- look at length of the votes array of the current task, then tell all of clients to move onto the next task (by sending an event; let them know)
+      // Then move onto the next task by updating the current task to increment to the next task in the taskList, and display that.
+      io.sockets.in(projectName).emit('votes', projects[projectName].votes)
+    });
+
+    socket.on('disconnect', function(){
+      projects[projectName].users--;
+
+      if(projects[projectName].users === 0) {
+        delete projects[projectName];
+      }
+    });
   });
 });
 
